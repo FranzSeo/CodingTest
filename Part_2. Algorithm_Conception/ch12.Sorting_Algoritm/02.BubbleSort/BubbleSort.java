c2250ec0eaf386f71471d14c80dc889e1fdf3a1d
Franz <lamoris135@gamil.com>
2023-06-28 17:12:45 +0900

    bubbleSort

--- Part_2. Algorithm_Conception/ch12.Sorting_Algoritm/02.BubbleSort/BubbleSort.java
+++ Part_2. Algorithm_Conception/ch12.Sorting_Algoritm/02.BubbleSort/BubbleSort.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
 public class BubbleSort {
-    public static void main(String[] args){
-        System.out.print(compare2number(4,2));
-        System.out.print(compare3number(4,8,1));
 
+    public static List<Integer> dataList;
+    public static void main(String[] args){
+        // System.out.print(compare2number(4,2));
+        // System.out.print(compare3number(4,8,1));
+        dataList = new ArrayList<>();
+        getData();
     }
     /*
      public static void sort(int[] arr) {
@@ -28,9 +31,23 @@ public class BubbleSort {
     } 
      */
     //Bubble Sort
-    public List<Integer> dataList;
+
+    public static List<Integer> getData(){
+        List<Integer> getList = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Insert data size : ");
+        int dataSize = scan.nextInt();
+
+        for(int i = 0; i < dataSize; i++){
+            int data = scan.nextInt();
+            getList.add(data);
+        }
+
+        return getList;
+    }
     
-    public List<Integer> bubbleSort(ArrayList<Integer> dataList){
+    public static List<Integer> bubbleSort(ArrayList<Integer> dataList){
         int endpoint = dataList.size() - 1;
         for(int i = endpoint; i > 0; i--){
             boolean swapped = false;
@@ -45,31 +62,31 @@ public class BubbleSort {
 
 
 
-    public static List<Integer> compare2number(int a, int b){
-        List<Integer> datas = new ArrayList<Integer>();
+    // public static List<Integer> compare2number(int a, int b){
+    //     List<Integer> datas = new ArrayList<Integer>();
 
-        datas.add(a);
-        datas.add(b);
+    //     datas.add(a);
+    //     datas.add(b);
 
-        if(datas.get(0) > datas.get(1)){
-            Collections.swap(datas, 0, 1);
-        }
-        return datas;
-    }
+    //     if(datas.get(0) > datas.get(1)){
+    //         Collections.swap(datas, 0, 1);
+    //     }
+    //     return datas;
+    // }
 
-    // 문제 발생 !!! 
-    public static List<Integer> compare3number(int a, int b, int c){
-        List<Integer> datas = new ArrayList<Integer>();
+    // // 문제 발생 !!! 
+    // public static List<Integer> compare3number(int a, int b, int c){
+    //     List<Integer> datas = new ArrayList<Integer>();
 
-        datas.add(a);
-        datas.add(b);
-        datas.add(c);
+    //     datas.add(a);
+    //     datas.add(b);
+    //     datas.add(c);
 
-        for(int i = 0; i < datas.size() - 1; i++){
-            if(datas.get(i) > datas.get(i + 1)){
-                Collections.swap(datas, i, i + 1);
-            }
-        }
-        return datas;
-    }
+    //     for(int i = 0; i < datas.size() - 1; i++){
+    //         if(datas.get(i) > datas.get(i + 1)){
+    //             Collections.swap(datas, i, i + 1);
+    //         }
+    //     }
+    //     return datas;
+    // }
 }
